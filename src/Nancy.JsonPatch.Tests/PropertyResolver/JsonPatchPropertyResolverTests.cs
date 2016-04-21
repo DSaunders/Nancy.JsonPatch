d70ac53e7f4d365e430347de56565c8fdb840e23
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-21 09:57:05 +0100

    Added test for the JsonPatchPropertyResolver.

---
+++ src/Nancy.JsonPatch.Tests/PropertyResolver/JsonPatchPropertyResolverTests.cs
@@ -0,0 +1,27 @@
+﻿using Nancy.JsonPatch.PropertyResolver;
+
+namespace Nancy.JsonPatch.Tests.PropertyResolver
+{
+    using Should;
+    using Xunit;
+
+    public class JsonPatchPropertyResolverTests
+    {
+        private readonly IJsonPatchPropertyResolver _propertyResolver;
+
+        public JsonPatchPropertyResolverTests()
+        {
+            _propertyResolver = new JsonPatchPropertyResolver();
+        }
+
+        [Fact]
+        public void Returns_Parameter()
+        {
+            const string propertyName = "TestProperty";
+
+            var result = _propertyResolver.Resolve(null, propertyName);
+
+            result.ShouldBeSameAs(propertyName);
+        }
+    }
+}
